c8eb18d62459206f96198c413d90f4b828e2a9f2
Matheus Gomes <matheusgomes062@gmail.com>
2024-05-26 16:57:30 -0300

    feat: add tooltip and handle open and close of sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import {
   File,
@@ -6,60 +8,107 @@ import {
   Settings,
   MessageSquareHeart,
   Plus,
+  ChevronFirst,
+  ChevronLast,
 } from "lucide-react";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
 
 export function Sidebar() {
+  const [expanded, setExpanded] = useState(true);
+
   return (
-    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex hover:w-56 transition-width duration-300">
-      <nav className="flex flex-col items-start gap-4 px-3 sm:py-5">
-        <Link
-          href="#"
-          className="group flex row h-9 w-19 shrink-0 items-center justify-between gap-2 rounded-full text-lg font-semibold text-secondary-foreground md:h-9 md:w-40"
-        >
-          <span className="sr-only">Melhor Currículo</span>
-          <span className="">Melhor Currículo</span>
-        </Link>
-        <Link
-          href="#"
-          className="transition-all hover:scale-105 flex h-9 w-9 items-center gap-3 px-2 bg-primary rounded-lg text-primary-foreground md:h-9 md:w-44"
-        >
-          <Plus className="h-5 w-5" />
-          <span className="sr-only">Criar Currículo</span>
-          <span className="">Criar Currículo</span>
-        </Link>
-        <Link
-          href="#"
-          className="flex h-9 w-9 items-center gap-3 px-2 rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-9 md:w-44"
-        >
-          <File className="h-5 w-5" />
-          <span className="sr-only">Dashboard</span>
-          <span>Dashboard</span>
-        </Link>
-        <Link
-          href="#"
-          className="flex h-9 w-9 items-center gap-3 px-2 rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-9 md:w-52"
-        >
-          <MessageSquareHeart className="h-5 w-5" />
-          <span className="sr-only">Análise Profissional</span>
-          <span>Análise Profissional</span>
-        </Link>
-        <Link
-          href="#"
-          className="flex h-9 w-9 items-center gap-3 px-2 rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-9 md:w-44"
-        >
-          <Folders className="h-5 w-5" />
-          <span className="sr-only">Exemplos</span>
-          <span>Exemplos</span>
-        </Link>
+    <aside
+      className={`fixed inset-y-0 left-0 z-10 flex-col border-r bg-background sm:flex transition-all duration-300 ${expanded ? "w-50" : "w-16"}`}
+    >
+      <nav className="flex flex-col items-start gap-4 px-3 py-5">
+        <div className={`flex row justify-between items-center px-2 ${expanded ? 'gap-3 w-full' : 'px-0'}`}>
+          <img
+            src="https://img.logoipsum.com/243.svg"
+            className={`overflow-hidden transition-all ${
+              expanded ? "w-28" : "w-0"
+            }`}
+            alt=""
+          />
+          <Button
+            onClick={() => setExpanded((curr) => !curr)}
+            className="flex items-center px-2 rounded-lg"
+            variant="outline"
+            size="icon"
+            >
+            {expanded ? <ChevronFirst /> : <ChevronLast />}
+          </Button>
+        </div>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link
+                href="#"
+                className={`hover:scale-105 flex items-center self-center px-2 bg-primary rounded-lg text-primary-foreground h-9 w-9 ${expanded ? 'w-48 gap-3' : ''}`}
+              >
+                <Plus className="h-5 w-5" />
+                <span className="sr-only">Criar Currículo</span>
+                <span className={`${expanded ? "block" : "hidden"}`}>Criar Currículo</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Criar Currículo</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link
+                href="#"
+                className={`flex h-9 w-9 items-center gap-3 px-2 rounded-lg text-muted-foreground transition-colors hover:text-foreground ${expanded ? 'h-9 w-44' : ''}`}
+              >
+                <File className="h-5 w-5" />
+                <span className="sr-only">Dashboard</span>
+                <span className={`${expanded ? "block" : "hidden"}`}>Dashboard</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Dashboard</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link
+                href="#"
+                className={`flex h-9 w-9 items-center gap-3 px-2 rounded-lg text-muted-foreground transition-colors hover:text-foreground h-9 w-9 ${expanded ? 'h-9 w-52' : ''}`}
+              >
+                <MessageSquareHeart className="h-5 w-5" />
+                <span className="sr-only">Análise Profissional</span>
+                <span className={`${expanded ? "block" : "hidden"}`}>Análise Profissional</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Análise Profissional</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link
+                href="#"
+                className={`flex h-9 w-9 items-center gap-3 px-2 rounded-lg text-muted-foreground transition-colors hover:text-foreground h-9 w-9 ${expanded ? 'h-9 w-44' : ''}`}
+              >
+                <Folders className="h-5 w-5" />
+                <span className="sr-only">Exemplos</span>
+                <span className={`${expanded ? "block" : "hidden"}`}>Exemplos</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">Exemplos</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </nav>
       <nav className="mt-auto flex flex-col items-start gap-4 px-3 sm:py-5">
         <Link
           href="#"
-          className="flex h-9 w-9 items-center gap-3 px-2 rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-9 md:w-44"
+          className={`flex h-9 w-9 items-center gap-3 px-2 rounded-lg text-muted-foreground transition-colors hover:text-foreground h-9 w-9 ${expanded ? 'h-9 w-44' : ''}`}
         >
           <Settings className="h-5 w-5" />
           <span className="sr-only">Settings</span>
-          <span>Settings</span>
+          <span className={`${expanded ? "block" : "hidden"}`}>Settings</span>
         </Link>
       </nav>
     </aside>
