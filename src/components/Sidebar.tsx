30683574c14005960d9a29ad353cbc810e8ed321
Matheus Gomes <matheusgomes062@gmail.com>
2024-05-26 15:25:13 -0300

    feat: remove tooltip and start to work on hover interactions

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,90 +13,61 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { Label } from "./ui/label";
 
 export function Sidebar() {
   return (
-    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex hover:w-48 transition-width duration-300">
+    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex hover:w-56 transition-width duration-300">
       <nav className="flex flex-col items-start gap-4 px-3 sm:py-5">
         <Link
           href="#"
-          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full outline text-lg font-semibold text-secondary-foreground md:h-8 md:w-8 md:text-base"
+          className="group flex row h-9 w-19 shrink-0 items-center justify-between gap-2 rounded-full text-lg font-semibold text-secondary-foreground md:h-9 md:w-40"
         >
-          <MSquare className="h-4 w-4 transition-all group-hover:scale-110" />
-          <span className="sr-only">Acme Inc</span>
+          <span className="sr-only">Melhor Currículo</span>
+          <span className="">Melhor Currículo</span>
         </Link>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className="flex h-9 w-9 items-center justify-center bg-primary rounded-lg text-primary-foreground md:h-8 md:w-8"
-              >
-                <Plus className="h-5 w-5" />
-                <span className="sr-only">Criar Currículo</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Criar Currículo</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
-                <File className="h-5 w-5" />
-                <span className="sr-only">Dashboard</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Dashboard</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
-                <MessageSquareHeart className="h-5 w-5" />
-                <span className="sr-only">Análise Profissional</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Análise Profissional</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
-                <Folders className="h-5 w-5" />
-                <span className="sr-only">Exemplos</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Exemplos</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        <Link
+          href="#"
+          className="transition-all hover:scale-105 flex h-9 w-9 items-center gap-3 px-2 bg-primary rounded-lg text-primary-foreground md:h-9 md:w-44"
+        >
+          <Plus className="h-5 w-5" />
+          <span className="sr-only">Criar Currículo</span>
+          <span className="">Criar Currículo</span>
+        </Link>
+        <Link
+          href="#"
+          className="flex h-9 w-9 items-center gap-3 px-2 rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-9 md:w-44"
+        >
+          <File className="h-5 w-5" />
+          <span className="sr-only">Dashboard</span>
+          <span>Dashboard</span>
+        </Link>
+        <Link
+          href="#"
+          className="flex h-9 w-9 items-center gap-3 px-2 rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-9 md:w-52"
+        >
+          <MessageSquareHeart className="h-5 w-5" />
+          <span className="sr-only">Análise Profissional</span>
+          <span>Análise Profissional</span>
+        </Link>
+        <Link
+          href="#"
+          className="flex h-9 w-9 items-center gap-3 px-2 rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-9 md:w-44"
+        >
+          <Folders className="h-5 w-5" />
+          <span className="sr-only">Exemplos</span>
+          <span>Exemplos</span>
+        </Link>
       </nav>
       <nav className="mt-auto flex flex-col items-start gap-4 px-3 sm:py-5">
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-              >
-                <Settings className="h-5 w-5" />
-                <span className="sr-only">Settings</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Settings</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        <Link
+          href="#"
+          className="flex h-9 w-9 items-center gap-3 px-2 rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-9 md:w-44"
+        >
+          <Settings className="h-5 w-5" />
+          <span className="sr-only">Settings</span>
+          <span>Settings</span>
+        </Link>
       </nav>
     </aside>
   );
