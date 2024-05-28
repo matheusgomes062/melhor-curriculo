95e784c60082446902595183152ad44b0154a192
Matheus Gomes <matheusgomes062@gmail.com>
2024-05-28 16:07:28 -0300

    fix: remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Sidebar } from "@/components/Sidebar";
-import Image from "next/image";
+import { Sidebar } from '@/components/Sidebar';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <Sidebar />
       <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3"></main>
     </div>
   );
